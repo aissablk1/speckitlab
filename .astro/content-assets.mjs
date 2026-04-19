53a6c4bef7b998551266157895d892cbb09ac90b
aissablk1 <87309911+aissablk1@users.noreply.github.com>
2026-04-20 01:33:31 +0200

    chore(gitignore): exclude dist/ and .astro/ build artefacts

--- .astro/content-assets.mjs
+++
@@ -1,5 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_zeeb4 from "../../assets/logo-light.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-import __ASTRO_IMAGE_IMPORT_Uws4c from "../../../assets/logo-light.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Ffr%2Findex.mdx";
-export default new Map([["../../assets/logo-light.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_zeeb4], ["../../../assets/logo-light.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Ffr%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Uws4c]]);
-		
